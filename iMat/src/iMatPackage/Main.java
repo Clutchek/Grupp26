72453fe65c9b34ef00e75aca9dba1bfad7daf042
Rektorn <nalexdotk@gmail.com>
2015-03-02 17:59:47 +0100

    Played around a bit with L&F

--- iMat/src/iMatPackage/Main.java
+++ iMat/src/iMatPackage/Main.java
@@ -1,6 +1,9 @@
 package iMatPackage;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,11 +17,32 @@ import javax.swing.JFrame;
  */
 public class Main {
     
-    public static void main(String[] args){
+    public static void main(String[] args) {
         StoreModel model = null;
         MainController controller = new MainController(model);
         StoreView storeview = new StoreView();
         controller.add(storeview);
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+            /*for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }*/
+        } 
+        catch (UnsupportedLookAndFeelException e) {
+            //UIManager.setLookAndFeel("javax.swing.plaf.metal.NimbusLookAndFeel");
+        }
+        catch (ClassNotFoundException e) {
+           //UIManager.setLookAndFeel("javax.swing.plaf.metal.NimbusLookAndFeel");
+        }
+        catch (InstantiationException e) {
+           //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        }
+        catch (IllegalAccessException e) {
+           //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        }
         controller.pack();
         controller.setVisible(true);
     }
