50359b9c8f0f3bed48aa1b3c4ac0797afd631bd1
Rektorn <nalexdotk@gmail.com>
2015-03-02 14:12:41 +0100

    Added main class for real this time

---
+++ iMat/src/iMatPackage/Main.java
@@ -0,0 +1,24 @@
+package iMatPackage;
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alexander
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        StoreModel model = new StoreModel();
+        MainController controller = new MainController(model);
+        StoreView storeview = new StoreView();
+        //controller.add(storeview);
+        controller.setVisible(true);
+    }
+}
